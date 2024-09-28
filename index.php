9ed4cf3ae17bc0566809b7330742b63473da5349
MamoonaQuddus <mamoonaquddusqadri@example.com>
2024-09-27 21:23:23 -0700

    Updated the background style

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
         
         <script>
+          // so why we use php html can not insert data into the database we firstly we have to pick up the data from the html in the php 
           // function validateForm() {
           
           //   var fname = document.forms["myForm"]["fname"].value;
@@ -84,42 +85,43 @@
       <div class="col-sm-6">
         
 
-        <form name="myForm" onsubmit="return validateForm()" action="./entryBiodata.Controller.php" method="POST" style="background-color:rgb(190, 215, 229); ">
+        <form name="myForm" onsubmit="return validateForm()" action="./entryBiodata.Controller.php" method="POST" 
+        style="background: rgba(0, 0, 0, 0.5); backdrop-filter: blur(10px); width: 600px; height: 650px; ">
           <div class="col-sm-12"> 
-          <h1 class="border-bottom center pb-3 mb-4" style="text-align: center;">Simple Form</h1>
+          <h1 class="border-bottom center pb-3 mb-4" style="text-align: center; color:white;">Form</h1>
         </div> 
 
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                    <label class="col-sm-3" for="firstName">First Name:</label>
+                    <label class="col-sm-3" for="firstName"  style="color:white;">First Name:</label>
                     <div class="col-sm-7">
                         <input type="text" name="fname" class="form-control" id="firstName" placeholder="First Name" required >
                     </div>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                    <label class="col-sm-3 " for="lastName">Last Name:</label>
+                    <label class="col-sm-3 " for="lastName" style="color:white;">Last Name:</label>
                     <div class="col-sm-7">
                         <input type="text" name="lname" class="form-control" id="lastName" placeholder="Last Name" required>
                     </div>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                    <label class="col-sm-3 " for="inputEmail">Email Address:</label>
+                    <label class="col-sm-3 " for="inputEmail" style="color:white;">Email Address:</label>
                     <div class="col-sm-7">
                         <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email Address" required>
                     </div>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                    <label class="col-sm-3 " for="phoneNumber">Mobile Number:</label>
+                    <label class="col-sm-3 " for="phoneNumber" style="color:white;">Mobile Number:</label>
                     <div class="col-sm-7">
                         <input type="number" name="phone" class="form-control" id="phoneNumber" placeholder="Phone Number" required>
                     </div>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                    <label class="col-sm-3">Date of Birth:</label>
+                    <label class="col-sm-3" style="color:white;">Date of Birth:</label>
                     <div class="col-sm-3">
                       <input type="date"  name="date" required>
                       </div>
@@ -129,20 +131,20 @@
                
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                    <label class="col-sm-3 ">Gender:</label>
+                    <label class="col-sm-3 " style="color:white;">Gender:</label>
                     <div class="col-sm-7 ">
-                        <label class="mb-0 mr-3">
-                            <input type="radio" class="mr-1" name="gender" value="male"> Male
+                        <label class="mb-0 mr-3" style="color:white;">
+                            <input type="radio" class="mr-1" name="gender" value="male" > Male
                         </label>
-                        <label class="mb-0 mr-3">
-                            <input type="radio" class="mr-1" name="gender" value="female"> Female
+                        <label class="mb-0 mr-3" style="color:white;">
+                            <input type="radio" class="mr-1" name="gender" value="female" > Female
                         </label>
                     </div>
                 </div>
 
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                  <label class="col-sm-3 " for="address">Address:</label>
+                  <label class="col-sm-3 " for="address" style="color:white;">Address:</label>
                   <div class="col-sm-7">
                       <input type="text" name="address" class="form-control" id="address" placeholder="Your Address" required>
                   </div>
@@ -150,31 +152,31 @@
 
                 <div class="form-group row">
                   <div class="col-sm-1"></div>
-                  <label class="col-sm-3 " >Hobbies:</label>
+                  <label class="col-sm-3 " style="color:white;" >Hobbies:</label>
                   <div class="col-sm-6">
                     <div class="custom-control custom-checkbox custom-control-inline">
                       <input class="custom-control-input" type="checkbox" id="exampleInlineCheckbox1" name="hobby[]" value="Reading Books">
-                      <label class="custom-control-label" for="exampleInlineCheckbox1">Reading Books</label>
+                      <label class="custom-control-label" for="exampleInlineCheckbox1" style="color:white;">Reading Books</label>
                     </div>
                     <div class="custom-control custom-checkbox custom-control-inline">
                       <input class="custom-control-input" type="checkbox" id="exampleInlineCheckbox2" name="hobby[]" value="Playing Basketball" >
-                      <label class="custom-control-label" for="exampleInlineCheckbox2">Playing Basketball</label>
+                      <label class="custom-control-label" for="exampleInlineCheckbox2" style="color:white;">Playing Basketball</label>
                     </div>
                     <div class="custom-control custom-checkbox custom-control-inline">
                       <input class="custom-control-input" type="checkbox" id="exampleInlineCheckbox3" name="hobby[]" value="Watching Movies" >
-                      <label class="custom-control-label" for="exampleInlineCheckbox3">Watching Movies</label>
+                      <label class="custom-control-label" for="exampleInlineCheckbox3" style="color:white;">Watching Movies</label>
                     </div>
                     <div class="custom-control custom-checkbox custom-control-inline">
                       <input class="custom-control-input" type="checkbox" id="exampleInlineCheckbox4" name="hobby[]" value="Playing Football" >
-                      <label class="custom-control-label" for="exampleInlineCheckbox4">Playing Football</label>
+                      <label class="custom-control-label" for="exampleInlineCheckbox4" style="color:white;">Playing Football</label>
                     </div>
                     <div class="custom-control custom-checkbox custom-control-inline">
                       <input class="custom-control-input" type="checkbox" id="exampleInlineCheckbox5" name="hobby[]" value="Listening to Musics" >
-                      <label class="custom-control-label" for="exampleInlineCheckbox5">Listening to Musics</label>
+                      <label class="custom-control-label" for="exampleInlineCheckbox5" style="color:white;">Listening to Musics</label>
                     </div>
                     <div class="custom-control custom-checkbox custom-control-inline">
                       <input class="custom-control-input" type="checkbox" id="exampleInlineCheckbox6" name="hobby[]" value="Cooking" >
-                      <label class="custom-control-label" for="exampleInlineCheckbox6">Cooking</label>
+                      <label class="custom-control-label" for="exampleInlineCheckbox6" style="color:white;">Cooking</label>
                     </div>
                   </div>
               </div>
